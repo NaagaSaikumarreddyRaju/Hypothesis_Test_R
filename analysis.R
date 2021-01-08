e728b5e1912d74978db932d8959723318a126871
Saikumar Reddy <sr19adr@herts.ac.uk>
2021-01-08 15:13:02 +0000

    Second Commit

--- analysis.R
+++ analysis.R
@@ -1,5 +1,11 @@
 install.packages("tidyverse")
 df <- read.csv("master.csv")
+
+# We've Performed Student's T test to Check dependency of Suicides Rate on Gender
+#Ho = "Gender has no effect on suicides"   # Stating the Null Hypothesis
+#Ha = "Gender has an effect on suicides"   # Stating the Alternate Hypothesis
+
 males_suicides=df[df['sex'] == 'male','suicides.100k.pop']
 females_suicides=df[df['sex'] == 'female','suicides.100k.pop']
+
 t.test(x=males_suicides,y=females_suicides,conf.level = 0.95,var.equal = TRUE)
